ab7419b27ccd68e7181c9944b6598a8fb69b8809
Anthony Lopez <anthonydlr66@gmail.com>
2023-04-10 13:59:49 -0400

    use of window was needed

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+var customerName = 'bob';
+var bestCustomer = undefined
+
+const LeastFavoriteCustomer = 'sheesh'
+
+function upperCaseCustomerName(customerName) {
+   window['customerName'] = 'BOB';
+      return customerName;
+}
+
+function setBestCustomer(bestCustomer) {
+   window['bestCustomer'] = 'not bob';
+      return bestCustomer;
+}
+
+function overwriteBestCustomer(bestCustomer) {
+   window['bestCustomer'] = 'maybe bob';
+      return bestCustomer;
+}
+
+function changeLeastFavoriteCustomer(LeastFavoriteCustomer) {
+   throw 'Assignment to constant variable.'
+}
